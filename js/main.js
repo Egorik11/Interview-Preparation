3481871dc0f4447afce98091d6488e458e86b49d
yehor <ivashin.egor98@gmail.com>
2023-03-10 13:26:39 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,45 +1,4 @@
-const questions = [
-  {
-    id: 1,
-    question: 'Explain event delegation.',
-    completed: false,
-  },
-  {
-    id: 2,
-    question: 'Explain how this works in JavaScript.',
-    completed: false,
-  },
-  {
-    id: 3,
-    question: 'Explain how prototypal inheritance works.',
-    completed: false,
-  },
-  {
-    id: 4,
-    question: 'What is a closure, and how/why would you use one?',
-    completed: false,
-  },
-  {
-    id: 5,
-    question: 'What s a typical use case for anonymous functions?',
-    completed: false,
-  },
-  {
-    id: 6,
-    question: 'What s the difference between host objects and native objects?',
-    completed: false,
-  },
-  {
-    id: 7,
-    question: 'Explain "hoisting".',
-    completed: false,
-  },
-  {
-    id: 8,
-    question: 'Describe event bubbling.',
-    completed: false,
-  },
-];
+import { questions } from './questions.js';
 
 function renderQuestions() {
   const listQuestion = document.querySelector('#listQuestion');
---
+++ js/questions.js
@@ -0,0 +1,34 @@
+export const questions = [
+  {
+    question: 'Explain event delegation.',
+    completed: false,
+  },
+  {
+    question: 'Explain how this works in JavaScript.',
+    completed: false,
+  },
+  {
+    question: 'Explain how prototypal inheritance works.',
+    completed: false,
+  },
+  {
+    question: 'What is a closure, and how/why would you use one?',
+    completed: false,
+  },
+  {
+    question: 'What s a typical use case for anonymous functions?',
+    completed: false,
+  },
+  {
+    question: 'What s the difference between host objects and native objects?',
+    completed: false,
+  },
+  {
+    question: 'Explain "hoisting".',
+    completed: false,
+  },
+  {
+    question: 'Describe event bubbling.',
+    completed: false,
+  },
+];
